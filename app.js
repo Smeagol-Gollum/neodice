c1ec925ff70eaeda99220bc5f75ffbb7d5d9ea22
Smeagol-Gollum <smeagol-gollum@users.users.noreply.github.com>
2015-07-28 14:47:28 -0400

    initial commit

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 487,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Untitled Dice',
+  app_name: 'neodice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  recaptcha_sitekey: '6Le3dQoTAAAAABZVe_2-XokNDzHGDUjXRL_wrcJk',  // <----- EDIT ME!
+  redirect_uri: 'https://smeagol-gollum.github.io/neodice',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: 'https://a-chat-server.herokuapp.com',
