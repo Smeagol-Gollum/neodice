8cd2c4c59688a9c5e8f47fc15a47ecc87c835af1
Smeagol-Gollum <smeagol-gollum@users.users.noreply.github.com>
2015-07-29 14:54:58 -0400

    Revert "fixes"
    This reverts commit 26a101264c73c1e94fc2c54644c1732e7e10c141.

--- app.js
+++ app.js
@@ -1996,10 +1996,10 @@ var UserBalanceBox = React.createClass({
             )
           ),
           el.div(
-            {className: 'pull-right text-right'},
+            {className: 'pull-right'},
             el.span(
               null,
-              'XP: ' + Math.floor((worldStore.state.user.betted_wager / 100) + worldStore.state.user.betted_count)
+              'XP: ' + Math.floor((worldStore.state.user.betted_wager / 100) * worldStore.state.user.betted_count)
               //'XP: ' + Math.floor(Math.sqrt((worldStore.state.user.betted_wager / (100)) * (worldStore.state.user.betted_count / 10)))
             ),
             el.br(),
@@ -2010,7 +2010,7 @@ var UserBalanceBox = React.createClass({
             el.br(),
             el.span(
               null,
-              worldStore.state.user.betted_count + ' bets'
+              worldStore.state.user.betted_count + ' bets | '
             )
           )
         );
