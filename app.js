87a4fea407e76f180da5e36cb34d7a99d2890c3c
Smeagol <Smeagol-Gollum@users.noreply.github.com>
2016-08-07 07:31:36 -0400

    change redirect uri from  .ga to .cf

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6Le3dQoTAAAAABZVe_2-XokNDzHGDUjXRL_wrcJk',  // <----- EDIT ME!
-  redirect_uri: 'http://neodice.ga',
+  redirect_uri: 'http://neodice.cf',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: 'https://a-chat-server.herokuapp.com',
@@ -1856,7 +1856,7 @@ var Footer = React.createClass({
       'copyright 2015 ',
       el.a(
         {
-          href: 'https://neodice.github.io'
+          href: 'http://neodice.cf'
         },
         'neodice'
       ),
